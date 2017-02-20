b03b563e42afd9b8b3872adfa18155af0e1f4dcb
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-02-20 21:12:41 +1200

    Publishing tupilabs.com

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DIR=`dirname "$0"`
-
-chef --root=$DIR purge
-chef --root=$DIR bake --output=blog --force
-#cp $DIR/CNAME blog/
-
-ghp-import -n -m "Publishing tupilabs.com" -p -r origin -b gh-pages blog 
