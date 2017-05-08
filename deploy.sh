e66c82c2afa0485f436c9e512f6de8c7eca344ae
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-05-08 20:23:04 +1200

    Add CNAME

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ chef --root=$DIR purge
 chef --root=$DIR bake --output=blog --force
 #cp $DIR/CNAME blog/
 
+cp "$DIR/CNAME" "$DIR/blog/"
+
 ghp-import -n -m "Publishing tupilabs.com" -p -r origin -b master blog 
