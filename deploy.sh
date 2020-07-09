d5308f842e780be98a773e1520d999e62135d203
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-07-09 21:34:15 +1200

    Fix shellcheck issues

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-DIR=`dirname "$0"`
+DIR=$(dirname "$0")
 
-chef --root=$DIR purge
-chef --root=$DIR bake --output=blog --force
+chef --root="$DIR" purge
+chef --root="$DIR" bake --output=blog --force
 #cp $DIR/CNAME blog/
 
 cp "$DIR/CNAME" "$DIR/blog/"
 
-ghp-import -n -m "Publishing tupilabs.com" -p -r origin -b master blog 
+ghp-import -n -m "Publishing tupilabs.com" -p -r origin -b master blog
